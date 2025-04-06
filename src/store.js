edd91304ebe6102e1646d2dd964d33694eeeeee8
leo-chartier <leo.chartier@dartybox.com>
2025-04-06 23:48:54 +0200

    Add example grid

--- src/store.js
+++ src/store.js
@@ -1,18 +1,34 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
+  // Notre Temps, grille n°2605, 6 avril 2025
   cells: [
-    [null, '', null, '', null],
-    ['', '', '', '', ''],
-    [null, '', '', '', ''],
-    ['', '', '', '', ''],
-    [null, '', '', '', ''],
+    [null, 'p', null, 'u', null, 'f'],
+    ['n', 'a', 'u', 's', 'e', 'e'],
+    [null, 't', 'r', 'a', 'c', 't'],
+    ['d', 'r', 'a', 'g', 'u', 'e'],
+    [null, 'i', 'n', 'e', 's', null],
+    ['p', 'o', 'i', 's', 's', 'e'],
+    [null, 't', 'u', null, 'o', 'u'],
+    ['c', 'e', 'm', 'e', 'n', 't'],
   ],
   defsSide: {
-    '0,0': 'side test',
+    '0,0': 'il est un peu chauvin',
+    '0,2': 'coutumes',
+    '0,4': "elle n'a pas l'air triste",
+    '2,0': 'feuille orientée',
+    '4,0': 'infante du portugal',
+    '6,0': 'gardé secret',
+    '6,3': "une question pour connâitre l'endroit",
   },
   defsBelow: {
-    '0,0': 'below test 1 2 3 4 5 6 7 8 9 very­long­word',
+    '0,0': 'malaise',
+    '0,2': 'il brule en centrale',
+    '0,4': 'blason cousu',
+    '2,0': 'branche en boîte',
+    '4,0': 'espèce de guigne',
+    '4,5': 'roula dans la farine',
+    '6,0': "protège l'ivoire à la racine",
   },
   lastRow: null,
   lastColumn: null,
